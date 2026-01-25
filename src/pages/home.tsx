137f72ee615f1c1c7fbdfee152d81e4f6c0f1af9
Andvrew <seyiababs@gmail.com>
2026-01-24 21:06:49 -0500

    almost ready for minimum deployment

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,10 +3,9 @@ import './home.css'
 function home() {
 
 return (
-<>
 <div className='h-screen grid place-items-center'>
-    <div className=' flex  ml-24 gap-[20vh] font-sans items-center'>
-      <h1>hi, im andrew babatunde</h1>
+    <div className=' relative bottom-12 flex gap-[15vh] font-sans items-center'>
+      <h1>hi, im andrew babatunde!</h1>
       
       {/* <a href='./music' >
         <p className=' text-white text-2xl font-bold underline'>music</p>
@@ -20,7 +19,8 @@ return (
       <p className=' text-white text-2xl font-bold underline'>github</p>
       </a>
       </div>
-      <div className='relative text-left mb-90 mr-90 '>
+
+      <div className='relative bottom-36 text-left mr-90 '>
          <p>aob85593 [at] uga [dot] edu
           <br />
           <br />im currently a junior at the university of georgia studying computer science <br />
@@ -33,10 +33,10 @@ return (
 
       </p>
        
-      <p className='text-white  mb-5'>currently enrolled @ <a href='https://computing.uga.edu'><span className='text-red-600'>UGA</span></a></p> 
+      <p className='text-white  mb-5'>currently enrolled @ <a href='https://computing.uga.edu'><span className='text-red-600'>the university of georgia</span></a></p> 
       {/* am I fucking stupid fix the georgia image moving to the right +
       I have never wrote a comment like this before what the heck*/}    
-<div>
+<div className='flex-col'>
 <h1 className='mb-1'>projects</h1>
 <a href='https://github.com/andrew-babatunde2004/cs1302-api-app'>
 <p className=' text-white underline mb-3 '>Internship Finder ↗ </p>
@@ -46,20 +46,21 @@ return (
 </a>
 <br />
 </div>
+<div>
 <h1> contributions </h1>
+<a href='https://github.com/DevDogs-UGA'>
 <p className='text-white underline'>DevDogs ↗</p>
+</a>
+</div>
 <br />
 <h1>leadership</h1>
 <p>Suwanne Youth Leaders ↗</p>
 <p>ColorStack ↗</p>
 <p>ACM ↗</p>
 <p>DevDogs ↗</p>
-      </div>
-      <footer>
-        <p className='text-white mt-auto min-h-screen bottom-20'>© 2025 Andrew Babatunde</p>
-      </footer>
-      </div>
-      </>
+   </div>
+        <p className='text-gray-500'>© 2025 Andrew Babatunde</p>
+</div>
   );
 };
 
